155567e958a8879df2e2fe358b1d3d21f7d85076
Ashley Qin <ashleyqin.wb@gmail.com>
2020-06-25 00:40:39 -0700

    Updates

---
+++ precache-manifest.8fa3b269e2aace8d75bf17f46c02483b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9630d0322168af3f40681f79308868e",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "4ebb093c017e21ece26e",
+    "url": "/MovieApp/static/css/main.9da65316.chunk.css"
+  },
+  {
+    "revision": "1736c391fcd867cdabf7",
+    "url": "/MovieApp/static/js/2.20b3b65e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieApp/static/js/2.20b3b65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebb093c017e21ece26e",
+    "url": "/MovieApp/static/js/main.c666dc54.chunk.js"
+  },
+  {
+    "revision": "516cbca2f979172b874b",
+    "url": "/MovieApp/static/js/runtime-main.c2a05db3.js"
+  }
+]);
